fd20629abcb9398246e8ac6ded0503130fece572
ALOK1407007 <37080859+ALOK1407007@users.noreply.github.com>
2023-03-04 12:18:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css'
+
+function App(){
+     return(
+          <div id='abc' class='menu-bar'>
+            <ul>
+              <li><a href="#"></a>Home</li>
+              <li><a href="#"></a>About us</li>
+              <li><a href="#"></a>Services</li>
+              <li><a href="#"></a>clients</li>
+              <li><a href="#"></a>Investors</li>
+              <li><a href="#"></a>Pricing</li>
+              <li><a href="#"></a>Training</li>
+            </ul>
+          </div>
+     )
+}
+export default App;
